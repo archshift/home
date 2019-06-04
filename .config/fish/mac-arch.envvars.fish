17b506589d80795d937ef95dd83e8fe722e37193
Gui Andrade <gh@archshift.com>
2019-06-03 22:13:12 -0700

    Add ruby to PATH

--- .config/fish/mac-arch.envvars.fish
+++ .config/fish/mac-arch.envvars.fish
@@ -2,6 +2,7 @@ set EDITOR nvim
 set INTEL_LICENSE_FILE $HOME/intel/LICENSE_VTUNE.lic
 set -gx PATH /usr/local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
+set -gx PATH ~/.gem/ruby/2.6.0/bin/ $PATH
 
 if test $XDG_CURRENT_DESKTOP != "kde"
     set -gx QT_QPA_PLATFORMTHEME 'qt5ct'
