9020c659b66b31fc109b34703821d8e71954e53c
Gui Andrade <gh@archshift.com>
2020-05-18 10:13:53 -0700

    Update config for arch-dsk envvars, i3

--- .config/fish/arch-dsk.envvars.fish
+++ .config/fish/arch-dsk.envvars.fish
@@ -16,3 +16,10 @@ set -x PATH $GOPATH/bin $PATH
 
 # Rust
 set -x RUST_SRC_PATH /usr/src/rust/src
+
+# Keystone
+set -x RISCV /opt/keystone-riscv
+set -x PATH /opt/keystone-riscv/bin $PATH
+set -x PATH /opt/riscv64-linux-musl-cross/bin $PATH
+set -x KEYSTONE_DIR ~/Dev/keystone
+set -x KEYSTONE_SDK_DIR ~/Dev/keystone/sdk
